2ec944d3e9939aff2cd3cca7e843b979d424d71a
hayunyun <hayunyun@likelion.org>
2022-11-15 09:46:22 +0900

    일반 지도만 띄우기

--- src/components/apt/AptCostVue.vue
+++ src/components/apt/AptCostVue.vue
@@ -66,7 +66,34 @@
 <script>
 
 export default {
+    
 
+    mounted() {
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement("script");
+            /* global kakao */
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+            "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=38e26e736d7ea2c5f9e89fa2b3a68b04&libraries=services";
+            document.head.appendChild(script);
+        }
+    }
+
+    , methods: {
+        initMap() {
+            const container = document.getElementById("map");
+            const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 5,
+        };
+
+        //지도 객체를 등록합니다.
+        //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
+            this.map = new kakao.maps.Map(container, options);
+        }
+    },
 }
 </script>
 
