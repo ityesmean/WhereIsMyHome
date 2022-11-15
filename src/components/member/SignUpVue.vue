433936af44dcdabafd488883d15e8ef9bfcfe791
ityesmean <ityesmean@naver.com>
2022-11-15 14:23:20 +0900

    회원가입 구현

--- src/components/member/SignUpVue.vue
+++ src/components/member/SignUpVue.vue
@@ -9,42 +9,46 @@
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-lg-5 bg-white p-5">
-                        <form action="" method="post" id="form-join"
-                            class="contact-form" data-aos="fade-up" data-aos-delay="200">
+                        <!-- <form @submit.prevent="submitForm" -->
+                            <!-- class="contact-form" data-aos="fade-up" data-aos-delay="200"> -->
                             <!-- <input type="hidden" name="action" value="insertmember"> -->
                             <div class="row">
                                 <div class="col-12">
                                     <div class="form-group">
                                         <label class="text-black" for="id">아이디</label> 
-                                        <input type="text" class="form-control" id="id" name="id">
+                                        <input v-model="id" type="text" class="form-control" id="id">
                                     </div>
                                     <div class="form-group">
                                         <label class="text-black" for="pw">비밀번호</label> 
-                                        <input type="password" class="form-control" id="pw" name="pw">
+                                        <input v-model="pw" type="password" class="form-control" id="pw" >
                                     </div>
                                     <div class="form-group">
                                         <label class="text-black" for="name">이름</label> 
-                                        <input type="text" class="form-control" id="name" name="name">
+                                        <input v-model="name" type="text" class="form-control" id="name" >
                                     </div>
                                     <div class="form-group">
                                         <label class="text-black" for="addr">주소</label> 
-                                        <input type="text" class="form-control" id="addr" name="addr">
+                                        <input v-model="addr" type="text" class="form-control" id="addr" >
                                     </div>
                                     <div class="form-group">
                                         <label class="text-black" for="phone">전화번호</label> 
-                                        <input type="text" class="form-control" id="phone" name="phone">
+                                        <input v-model="phone" type="text" class="form-control" id="phone">
                                     </div>
                                 </div>
                             </div>
 
-                            <input type="button" id="btn-join" class="btn btn-primary mb-4" value="등록">
+                            <router-link to="/member/login">
+                                <button id="btn-join" @click="joinMember" class="btn btn-primary mb-4">
+                                    등록
+                                </button>
+                            </router-link>
 
                             <div class="form-group">
                                 <p>
                                     <small>이미 계정이 있으시다면, <router-link to="/login">로그인</router-link></small>
                                 </p>
                             </div>
-                        </form>
+                        <!-- </form> -->
                     </div>
                     <!-- /.col-lg-7 -->
                 </div>
@@ -60,14 +64,27 @@ export default {
     name: 'SignUpVue',
     data: function() {
         return {
-            member: ''
+            id: null,
+            pw: null,
+            name: null,
+            addr: null,
+            phone: null,
         }
     },
     created: function() {
         this.joinMember()
     },
     methods: {
-        joinMember: function() {
+        joinMember: function () {
+
+            
+            // console.log(this.id);
+            // console.log(this.pw);
+            // console.log(this.name);
+            // console.log(this.addr);
+            // console.log(this.phone);
+
+
             this.$axios.post('http://localhost/admin/member', {
                 id: this.id,
                 pw: this.pw,
@@ -78,7 +95,7 @@ export default {
                 .then(response => {
                     console.log(response)
                     this.$router.push({
-                        name: "member"
+                        name: "login"
                     });
                 })
                 .catch(error => {
