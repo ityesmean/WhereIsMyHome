6669ba424750af4b142fce73eb8a9adc5fac6b16
이승민 <ityesmean@naver.com>
2022-11-19 02:03:48 +0900

    로그아웃 버튼 수정

--- src/components/common/HeaderVue.vue
+++ src/components/common/HeaderVue.vue
@@ -21,21 +21,21 @@
             <div class="site-navigation">
                 <a href="/" class="logo m-0">구해줘, 홈즈<span class="text-primary">.</span></a>
                 <!-- <router-link to="/" class="logo m-0">구해줘, 홈즈<span class="text-primary">.</span></router-link> -->
+                <ul class="js-clone-nav d-none d-lg-inline-block text-left float-right site-menu">
                 <template v-if="sessionData == null">
-                  <ul class="js-clone-nav d-none d-lg-inline-block text-left float-right site-menu">
                       <li><router-link to="/member/signup">Sign up</router-link></li> 
                       <li class="cta-button active"><router-link to="/member/login">Login</router-link></li>      
-                  </ul>
+                
                 </template> 
                 <template v-else>
-                  <ul class="js-clone-nav d-none d-lg-inline-block text-left float-right site-menu">
+                  
                     <li><router-link to="/board/boardnotice">Notice</router-link></li> 
                     <li><router-link to="/member/mypage">My Page</router-link></li> 
                     <li><router-link to="/board/boardlist">Board</router-link></li> 
-                    <li class="cta-button active"><button class="btn btn-primary mb-4" @click="logout">logout</button></li>  
+                    <li class="cta-button active"><a style="cursor:pointer" @click="logout">logout</a></li>  
                     <!-- <li class="cta-button active"> <button id="" to="/member/login" @click="logout">LogOut</button></li>   -->
-                  </ul>
                 </template>
+                  </ul>
 
                 <a href="#" class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none" data-toggle="collapse" data-target="#main-navbar">
                 <span></span>
